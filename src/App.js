a4d660b4680c8357148400cde43a0c141fc5ca03
James Maddox <james.maddox07@gmail.com>
2020-06-10 12:29:31 -0400

    added parcel, extracted pet component into separate file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-// your code is going to go here for now
-const Pet = ({ name, animal, breed }) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed),
-    ])
-}
+import React from "react";
+import { render } from "react-dom";
+import { Pet } from "./Pet";
 
 const App = () => {
-    return React.createElement(
-        "div",
-        { id: "something-important" },
-        [
-            React.createElement('h1', {}, "Adopt Me!"),
-            React.createElement(Pet, { name: "Cocoa", animal: "Dog", breed: "Pitbull Mix"}),
-            React.createElement(Pet, { name: "Buddy", animal: "Dog", breed: "German Shepherd"}),
-            React.createElement(Pet, { name: "Harmony", animal: "Cat", breed: "Tabby"}),
-        ]
-    )
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Cocoa",
+      animal: "Dog",
+      breed: "Pitbull Mix",
+    }),
+    React.createElement(Pet, {
+      name: "Buddy",
+      animal: "Dog",
+      breed: "German Shepherd",
+    }),
+    React.createElement(Pet, {
+      name: "Harmony",
+      animal: "Cat",
+      breed: "Tabby",
+    }),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+render(React.createElement(App), document.getElementById("root"));
