a73c94cec779abaa963d47a7222d91b7f829c228
James Maddox <james.maddox07@gmail.com>
2020-06-17 15:27:14 -0400

    code splitting for faster load time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 import NavBar from "./NavBar";
+
+// details is a placeholder component that wont load the code until later when details needs to be rendered for the first time
+// This is called a dynamic import (JS thing, not React)
+// Parcel splits into a seperate bundle
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const theme = useState("darkorange");
@@ -14,10 +18,12 @@ const App = () => {
       <ThemeContext.Provider value={theme}>
         <div>
           <NavBar />
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
+          <Suspense fallback={<h1>loading route ...</h1>}>
+            <Router>
+              <SearchParams path="/" />
+              <Details path="/details/:id" />
+            </Router>
+          </Suspense>
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
